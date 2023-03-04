42bc59431b0d45c6d1cc8a4ed271d6f4a7432289
nawam727 <nawamdenawakage@gmail.com>
2023-03-04 22:32:55 +0530

    Adding hero section to the website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import { styles } from '../style';
+import { styles } from '../styles';
 import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
