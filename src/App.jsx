e5c89206748ba6d1c310bb6049ecdb2cfea9463e
nawam727 <nawamdenawakage@gmail.com>
2023-03-04 10:01:38 +0530

    Adding neccesory files to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <div>
       3D Developer Portfolio
