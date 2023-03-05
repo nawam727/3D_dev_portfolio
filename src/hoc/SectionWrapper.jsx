c810f28abcea5adc732558279744298bd7d644a5
nawam727 <nawamdenawakage@gmail.com>
2023-03-05 16:45:02 +0530

    Adding about section to the website part 2

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -5,7 +5,17 @@ import { staggerContainer } from '../utils/motion';
 const SectionWrapper = (Component, idName) => 
 function HOC () {
   return (
-    <motion.section>
+    <motion.section
+        variants={staggerContainer()}
+        initial="hidden"
+        whileInView="show"
+        viewport={{once: true, amount:0.25 }}
+        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+    >
+
+        <span className='hash-span' id={idName}>
+            &nbsp;
+        </span>
         <Component />
     </motion.section>
   )
